a20e4df5b11bf9e7c9065dc733aff9f481ba52ad
mabin <jansan_godsend@sina.com>
2018-11-29 20:05:06 +0800

    地址管理列表

--- framework7-air-cleaner-customer-vue-webpack-master/src/pages/home.vue
+++ framework7-air-cleaner-customer-vue-webpack-master/src/pages/home.vue
@@ -3,6 +3,7 @@
     <f7-list>
       <f7-list-item link="/center/" title="个人中心"></f7-list-item>
       <f7-list-item link="/form/" title="注册登录"></f7-list-item>
+      <f7-list-item link="/address/" title="地址管理"></f7-list-item>
     </f7-list>
   </f7-page>
 </template>
