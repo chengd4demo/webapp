b51d1a23d2814f3c4ecc8e54d42440094fc5eb81
mabin <jansan_godsend@sina.com>
2018-11-30 11:23:46 +0800

    add

---
+++ framework7-air-cleaner-customer-vue-webpack-master/src/pages/address.vue
@@ -0,0 +1,59 @@
+<template>
+  <f7-page>
+     <f7-navbar title="地址管理" back-link=""></f7-navbar>
+     
+     <div class="list" contacts="">
+        <div class="list-group">
+          <ul>
+            <li class="">
+              <div class="item-content">
+                <div class="item-inner">
+                    <div class="item-title">Aaron</div>
+                    <div>18611111111</div> 
+                    <div style="font-size: 12px;">
+                      <span style="background-color: rgb(67, 207, 124); color: rgb(255, 255, 255);">默认</span>
+                    </div> 
+                    <a href="/center/" class="button">修改</a>
+                </div>
+              </div>
+              <div class="address">静安中心秘密开发基地 666</div>
+            </li>
+          </ul>
+           <ul>
+            <li class="">
+              <div class="item-content">
+                <div class="item-inner">
+                    <div class="item-title">Aaron</div>
+                    <div>18611111111</div> 
+                    <div style="font-size: 12px;">
+                      <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                    </div> 
+                    <a href="/center/" class="button">修改</a>
+                </div>
+              </div>
+              <div class="address">环球中心N5 109999</div>
+            </li>
+          </ul>
+        </div>
+    </div>
+    <div class="fab fab-right-bottom color-purple">
+    <!--1:定义悬浮按钮UI样式-->
+    <a href="/abount/">
+      <i class="icon f7-icons  icon-plus">add</I>
+    
+    </a>
+
+   
+  </div>
+  </f7-page>
+</template>
+<style>
+  .address{
+        padding-left:16px;
+        color:#D5D5D5;
+  }
+</style>
+<script>
+
+
+</script>
