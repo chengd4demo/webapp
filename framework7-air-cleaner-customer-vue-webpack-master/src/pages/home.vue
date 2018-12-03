4e72ef969ca9eb15edf9721fbaf7ebabf88b84d4
chengd5demo <229462098@qq.com>
2018-12-03 20:19:55 +0800

    update

--- framework7-air-cleaner-customer-vue-webpack-master/src/pages/home.vue
+++ framework7-air-cleaner-customer-vue-webpack-master/src/pages/home.vue
@@ -1,21 +1,20 @@
 <template>
   <f7-page>
     <f7-list>
-      <f7-list-item link="/center/" title="个人中心"></f7-list-item>
-      <f7-list-item link="/form/" title="注册登录"></f7-list-item>
-	    <f7-list-item link="/price/" title="支付页面"></f7-list-item>
-      <f7-list-item link="/address/" title="地址管理"></f7-list-item>
-<<<<<<< HEAD
-      <f7-list-item link="/record/" title="使用记录"></f7-list-item>
-      <f7-list-item link="/equipment-list/" title="设备列表"></f7-list-item>
-      <f7-list-item link="/monitor/" title="设备监控页面"></f7-list-item>
-      <f7-list-item link="/state/" title="提示"></f7-list-item>
-=======
-       <f7-list-item link="/sign/" title="注册登录"></f7-list-item>
->>>>>>> 8a022452535309c1a6952f0a32578ff35195384b
+      <!--<f7-list-item link="/center/" title="个人中心"></f7-list-item>-->
+      <!--<f7-list-item link="/form/" title="注册登录"></f7-list-item>-->
+	    <!--<f7-list-item link="/price/" title="支付页面"></f7-list-item>-->
+      <!--<f7-list-item link="/address/" title="地址管理"></f7-list-item>-->
+
+      <!--<f7-list-item link="/record/" title="使用记录"></f7-list-item>-->
+      <!--<f7-list-item link="/equipment-list/" title="设备列表"></f7-list-item>-->
+      <!--<f7-list-item link="/monitor/" title="设备监控页面"></f7-list-item>-->
+      <!--<f7-list-item link="/state/" title="提示"></f7-list-item>-->
+       <!--<f7-list-item link="/sign/" title="注册登录"></f7-list-item>-->
+
     </f7-list>
   </f7-page>
 </template>
 <script>
-export default {}
+
 </script>
