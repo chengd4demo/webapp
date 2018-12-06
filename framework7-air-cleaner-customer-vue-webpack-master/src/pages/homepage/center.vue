a659ee1fe21c025210d39a0fa53708405c94e8a2
snoweaglehym <537045246@qq.com>
2018-12-06 11:12:12 +0800

    样式修改

--- framework7-air-cleaner-customer-vue-webpack-master/src/pages/homepage/center.vue
+++ framework7-air-cleaner-customer-vue-webpack-master/src/pages/homepage/center.vue
@@ -1,126 +1,155 @@
 <!-- center Page Template -->
 <template id="page-center">
-   
-    <f7-page no-navbar>
-          <div class="center">个人中心</div>
-        <f7-list media-list class="no-margin-v" style="margin-top:1px">  
-            <ul class="no-border-v">            
-                <li>
-                    <a href="/personal-information/" class="item-link">
-                        <div class="item-content" style="background-color:#E94E24;">
-                            <div class="item-media">
-                                <img src='http://img2.imgtn.bdimg.com/it/u=1932439000,2358402856&fm=26&gp=0.jpg' style="max-width:80px;max-height:800px;border-radius:50%">
-                                <i class="icon fa fa-opencart bg-red color-white align-center" style="font-size: 36px;width: 60px;height: 60px;line-height: 60px;border-radius: 50%;"></i>
-                            </div>
-                           <div class="item-inner">
-                                <div class="item-title">&nbsp;</div>
-                                <div class="item-title-row">
-                                    <div class="item-subtitle item-title" style="margin-left:-55px;color:white">冬天的萝卜</div>
-                                </div>
-                                <!--<div class="item-text" style="margin-left:-55px;color:	white">￥ 1000</div>-->
-                            </div>
-                        </div>
-                    </a>
-                </li>
-            </ul>
 
-        </f7-list >
-        <f7-row>
-          <div class="center-1">
-              <div class="center-item">
-                <div class="center-item-image" >
-                  <img src="../../img/msg.png">
-                </div>
-               <div class="center-item-title">消息</div>
-              </div>
-              <div class="center-item">
-                <a href="/equipment-list/"><div class="center-item-image">
-                  <img src="../../img/shebei.png">
-                </div>
-                <div class="center-item-title">设备监控</div></a>
-              </div>
-              <div class="center-item-1">
-                <a href="/record/"><div class="center-item-image">
-                  <img src="../../img/zb.png">
-                </div>
-                  <div class="center-item-title">使用记录</div></a>
-              </div>
-          </div>
-        </f7-row>
-        <f7-list media-list class="no-margin-v" style="margin-top:1px ;;">
+	<f7-page no-navbar>
+		<div class="center">个人中心</div>
+		<f7-list media-list class="no-margin-v" style="margin-top:1px">
+			<ul class="no-border-v">
+				<li>
+					<a href="/personal-information/" class="item-link">
+						<div class="item-content" style="background-color:#E94E24;">
+							<div class="item-media">
+								<img src='http://img2.imgtn.bdimg.com/it/u=1932439000,2358402856&fm=26&gp=0.jpg' style="max-width:80px;max-height:800px;border-radius:50%">
+								<i class="icon fa fa-opencart bg-red color-white align-center" style="font-size: 36px;width: 60px;height: 60px;line-height: 60px;border-radius: 50%;"></i>
+							</div>
+							<div class="item-inner">
+								<div class="item-title">&nbsp;</div>
+								<div class="item-title-row">
+									<div class="item-subtitle item-title" style="margin-left:-55px;color:white">冬天的萝卜</div>
+								</div>
+								<!--<div class="item-text" style="margin-left:-55px;color:	white">￥ 1000</div>-->
+							</div>
+						</div>
+					</a>
+				</li>
+			</ul>
 
-          <div class="jump-list"><img src="../../img/youhuijuan.png"><f7-list-item class="jump-list-title" link="/center/" title="优惠"></f7-list-item></div>
-          <div class="jump-list"><img src="../../img/kehu.png"><f7-list-item class="jump-list-title" link="/center/" title="在线客服"></f7-list-item></div>
-          <div class="jump-list"><img src="../../img/map.png"><f7-list-item class="jump-list-title" link="/center/" title="地图"></f7-list-item></div>
-          <div class="jump-list"><img src="../../img/dizhi.png"> <f7-list-item class="jump-list-title" link="/address/" title="地址管理"></f7-list-item></div>
-          <div class="jump-list"><img src="../../img/jiangping.png"><f7-list-item class="jump-list-title" link="/center/" title="我的奖品"></f7-list-item></div>
-        </f7-list>
-    </f7-page>
+		</f7-list>
+		<f7-row>
+			<div class="center-1">
+				<div class="center-item">
+					<div class="center-item-image">
+						<img src="../../img/msg.png">
+					</div>
+					<div class="center-item-title">消息</div>
+				</div>
+				<div class="center-item">
+					<a href="/equipment-list/">
+						<div class="center-item-image">
+							<img src="../../img/shebei.png">
+						</div>
+						<div class="center-item-title">设备监控</div>
+					</a>
+				</div>
+				<div class="center-item-1">
+					<a href="/record/">
+						<div class="center-item-image">
+							<img src="../../img/zb.png">
+						</div>
+						<div class="center-item-title">使用记录</div>
+					</a>
+				</div>
+			</div>
+		</f7-row>
+		<f7-list media-list class="no-margin-v" style="margin-top:1px ;;">
+
+			<div class="jump-list"><img src="../../img/youhuijuan.png">
+				<f7-list-item class="jump-list-title" link="/center/" title="优惠"></f7-list-item>
+			</div>
+			<div class="jump-list"><img src="../../img/kehu.png">
+				<f7-list-item class="jump-list-title" link="/center/" title="在线客服"></f7-list-item>
+			</div>
+			<div class="jump-list"><img src="../../img/map.png">
+				<f7-list-item class="jump-list-title" link="/center/" title="地图"></f7-list-item>
+			</div>
+			<div class="jump-list"><img src="../../img/dizhi.png">
+				<f7-list-item class="jump-list-title" link="/address/" title="地址管理"></f7-list-item>
+			</div>
+			<div class="jump-list"><img src="../../img/jiangping.png">
+				<f7-list-item class="jump-list-title" link="/center/" title="我的奖品"></f7-list-item>
+			</div>
+		</f7-list>
+	</f7-page>
 </template>
 <script>
 
 </script>
 <style type="text/css">
-img {
-  width:50px;
-  height:50px;
-}
-.center{
-  width: 100%;
-  height: 50px;
-  line-height: 50px;
-  font-size: 18px;
-  color: white;
-  font-family: 微软雅黑;
-  background: #E94E24;
-  text-align: center;
-}
-.center-1{
-  width: 100%;
-  height: 80px;
-  margin-top: -32px;
-  display: flex;
-  margin-bottom: -1px;
-}
-.center-item{
-  width: 34%;
-  height: 80px;
-  border-right: 1px solid #CDCDCD;
-  text-align: center;
+	.ios .media-list .item-title,
+	.ios li.media-item .item-title {
+		font-weight: normal;
+	}
 
-}
-.center-item-1{
-  width: 34%;
-  height: 80px;
-  text-align: center;
+	img {
+		width: 50px;
+		height: 50px;
+	}
 
-}
-.center-item-image{
-  width: 40px;
-  height: 40px;
-  margin: 5px auto;
-}
-.center-item-image img{
-  width: 32px;
-  height: 32px;
-}
-.center-item-title{
-  width: 100%;
-  color: black;
-  margin-top: 5px;
-}
-.jump-list{
-  list-style: none;
-  border: 1px solid #E9E9E9;
-}
-.jump-list-title{
-  margin-left: 30px;
-}
-.jump-list img {
-  width: 15px;
-  height: 15px;
-  position: fixed;
-  margin-top: 18px;
-  margin-left: 15px;
-}
+	.center {
+		width: 100%;
+		height: 50px;
+		line-height: 50px;
+		font-size: 18px;
+		color: white;
+		font-family: 微软雅黑;
+		background: #E94E24;
+		text-align: center;
+	}
+
+	.center-1 {
+		width: 100%;
+		height: 80px;
+		margin-top: -32px;
+		display: flex;
+		margin-bottom: -1px;
+	}
+
+	.center-item {
+		width: 34%;
+		height: 80px;
+		border-right: 1px solid #CDCDCD;
+		text-align: center;
+
+	}
+
+	.center-item-1 {
+		width: 34%;
+		height: 80px;
+		text-align: center;
+
+	}
+
+	.center-item-image {
+		width: 40px;
+		height: 40px;
+		margin: 5px auto;
+	}
+
+	.center-item-image img {
+		width: 32px;
+		height: 32px;
+	}
+
+	.center-item-title {
+		width: 100%;
+		color: black;
+		margin-top: 5px;
+	}
+
+	.jump-list {
+		list-style: none;
+		border: 1px solid #E9E9E9;
+	}
+
+	.jump-list-title {
+		margin-left: 30px;
+	}
+
+	.jump-list img {
+		width: 15px;
+		height: 15px;
+		position: fixed;
+		margin-top: 14px;
+		margin-left: 15px;
+	}
 </style>
--- framework7-air-cleaner-customer-vue-webpack-master/src/pages/sign.vue
+++ framework7-air-cleaner-customer-vue-webpack-master/src/pages/sign.vue
@@ -1,99 +1,114 @@
 <template>
-  <f7-page>
-    <div class="header-logo">
-      <img src="../img/img2039716589809cc0.png">
-      <p>圈兔共享空气净化器</p>
-    </div>
-    <div style="margin-top:10px;color:#e94e24;">
-          <span>圈兔共享空气净化器</span>
-    </div>
-</div>
- <f7-list form>
-		<f7-list-item >
-			<f7-input type="number" placeholder="输入手机号码" style="height: 44px;"/>
-		</f7-list-item>
-  <f7-list-item style="height: 44px;">
-			<f7-col style="height: 44px;">
-				<f7-input type="number" placeholder="验证码"/>
-			</f7-col>
-			<f7-col style="height: 44px;">
-					<a href="#" id="code" @click="countDown" class= "button button-fill" :class="{disabled: !this.canClick}" no-fast-click style="margin:0 auto;background:#e94e24;disable:disable;height: 44px; line-height:44px;">{{content}}</a>
-			</f7-col>
-  </f7-list-item>
-  <f7-list-item checkbox name="my-checkbox" value="2" title="我已阅读,并同意《用户注册协议》" style="height: 44px;font-size: 12px;"></f7-list-item>  
-  <p><a href="/center/" class="button button-fill" style="width:90%;margin:0 auto;background:#e94e24;height: 44px; line-height:44px;">验证手机</a></p>
- </f7-list>
-</f7-page>
+	<f7-page>
+		<div class="header-logo">
+			<img src="../img/img2039716589809cc0.png">
+			<p>圈兔圈共享空气净化器</p>
+		</div>
+		</div>
+		<div>
+			<form>
+				<div style="height: 44px; line-height: 44px; width: 100%; background: #fff;border-bottom: 1px solid #ccc;">
+					<input type="number" placeholder="输入手机号码" style="height: 44px; line-height: 44px;  text-indent: 10px;"/>
+				</div>
+				<div style="height: 44px; line-height: 44px; width: 100%; background: #fff;border-bottom: 1px solid #ccc;">
+					<span style="width:70%;float: left;">
+						<input type="number" placeholder="输入验证码" style="height: 44px; line-height: 44px;  text-indent: 10px;"/>
+					</span>
+					<span style="width: 30%; ">
+						<a href="#" id="code" @click="countDown" class="button button-fill" :class="{disabled: !this.canClick}"
+						no-fast-click style="margin:0 auto;background:#e94e24;disable:disable;height: 44px; line-height:44px;">{{content}}</a>
+					</span>
+				</div>
+				<div style="height: 44px; line-height: 44px; width: 100%; background: #fff;">
+					<span style="width:7%;float: left;text-align: left;">
+						<input type="checkbox" name="my-checkbox" value="2"  style="height: 38px; line-height: 40px; "/>
+					</span>
+					<span style="width: 93%; ">
+						<a href="#">我已阅读,并同意《用户注册协议》</a>
+					</span>
+				</div>
+				<div style="width: 100%; margin-top: 20px;">
+					<a href="/center/" class="button button-fill" style="width:90%;margin:0 auto;background:#e94e24;height: 44px; line-height:44px;">验证手机</a>
+				</div>
+			</form>
+		</div>
+	</f7-page>
 </template>
 <style>
-    .disabled{
-        background-color: #ddd;
-        border-color: #ddd;
-        color:#57a3f3;
-        cursor: not-allowed;
-    }
-		input::-webkit-input-placeholder {
-         /* placeholder颜色  */
-         color: #aab2bd;
-         /* placeholder字体大小  */
-         font-size: 12px;
-     }
-		 .ios .icon-checkbox, .ios .checkbox i {
-				width: 18px;
-				height: 18px;
-				/* border-radius: 50%; */
-				border: 1px solid #c7c7cc;
-		}
-  .content-block{
-    margin:0 auto 0 0;
-    border: 0px solid white !important;
-    font-size: 17px;
-    color: #b9b9b9 !important;
-    padding: 0px;
-  }
-  .header-logo{
-    width: 100%;
-    height: 175px;
-    text-align: center;
-  }
-  .header-logo img{
-    width: 160px;
-    height: 70px;
-    margin-top: 25px;
-  }
-  .header-logo p{
-    color: #E94E24;
-    margin-top: 20px;
-    font-size: 16px;
-  }
+	.disabled {
+		background-color: #ddd;
+		border-color: #ddd;
+		color: #57a3f3;
+		cursor: not-allowed;
+	}
+
+	input::-webkit-input-placeholder {
+		/* placeholder颜色  */
+		color: #aab2bd;
+		/* placeholder字体大小  */
+		font-size: 12px;
+	}
+
+	.ios .icon-checkbox,
+	.ios .checkbox i {
+		width: 18px;
+		height: 18px;
+		/* border-radius: 50%; */
+		border: 1px solid #c7c7cc;
+	}
+
+	.content-block {
+		margin: 0 auto 0 0;
+		border: 0px solid white !important;
+		font-size: 17px;
+		color: #b9b9b9 !important;
+		padding: 0px;
+	}
+
+	.header-logo {
+		width: 100%;
+		height: 175px;
+		text-align: center;
+	}
+
+	.header-logo img {
+		width: 160px;
+		height: 70px;
+		margin-top: 25px;
+	}
+
+	.header-logo p {
+		color: #E94E24;
+		margin-top: 20px;
+		font-size: 16px;
+	}
 </style>
 
 <script>
-  export default{
-    data() {
-      return {
-        content: '发送验证码',
-        totalTime: 60,
-        canClick:true
-      }
-    },
-    methods: {
-      countDown() {
-        if (!this.canClick) return
-        this.canClick = false
-        this.content = this.totalTime + 's后重新发送'
-        let clock = window.setInterval(() => {
-          this.totalTime --
-          this.content = this.totalTime + 's后重新发送'
-          if(this.totalTime <0) {
-            window.clearInterval(clock);
-            this.content = '重新发送验证码'
-            this.totalTime = 60
-            this.canClick = true
-          }
-        },1000)
-      },
-    }
-  }
-
+	export default {
+		data() {
+			return {
+				content: '发送验证码',
+				totalTime: 60,
+				canClick: true
+			}
+		},
+		methods: {
+			countDown() {
+				if (!this.canClick) return
+				this.canClick = false
+				this.content = this.totalTime + 's后重新发送'
+				let clock = window.setInterval(() => {
+					this.totalTime--
+					this.content = this.totalTime + 's后重新发送'
+					if (this.totalTime < 0) {
+						window.clearInterval(clock);
+						this.content = '重新发送验证码'
+						this.totalTime = 60
+						this.canClick = true
+					}
+				}, 1000)
+			},
+		}
+	}
 </script>
