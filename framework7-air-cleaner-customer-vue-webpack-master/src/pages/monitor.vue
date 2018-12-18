35fdd01f43f6cc40fa0c5dc3185af9c288056511
chengd5demo <229462098@qq.com>
2018-12-18 09:30:07 +0800

    消费者端布局优化

--- framework7-air-cleaner-customer-vue-webpack-master/src/pages/monitor.vue
+++ framework7-air-cleaner-customer-vue-webpack-master/src/pages/monitor.vue
@@ -2,27 +2,33 @@
 <template id="page-center">
 
   <f7-page>
-    <f7-navbar class="header-title" title="我的PM2.5" back-link="" style="background-color:rgba(0,0,255,0.2);"></f7-navbar>
+    <f7-navbar class="header-title" title="我的PM2.5" back-link="" style="background:#E94E24;"></f7-navbar>
     <div class="header-quality">
       <div class="air-quality">55</div>
       <p class="air-condition">空气状况:良好</p>
       <p class="health-advice">健康建议:极少数敏感人群应减少户外运动</p>
     </div>
     <div class="running-state">
-      <p style="margin-left: 50px">设备号:<span>{{machNo}}</span></p>
-      <p>设备运行状态:<span>正常</span></p>
-      <p>设备运行时长:<span>{{costTime}}</span></p>
-      <p>剩余运行时长:<span>{{lastTime}}</span></p>
+      <table>
+        <tr>
+          <td>设备号:</td>
+          <td><span class="span">{{machNo}}</span></td>
+        </tr>
+        <tr>
+          <td>设备运行状态:</td>
+          <td><span class="span">正常</span></td>
+        </tr>
+        <tr>
+          <td>设备运行时长:</td>
+          <td><span class="span">{{costTime}}</span></td>
+        </tr>
+        <tr>
+          <td>剩余运行时长:</td>
+          <td><span class="span">{{lastTime}}</span></td>
+        </tr>
+      </table>
     </div>
     <p style="margin-top: 50px;"><a :href="getPriceHref()" class="button button-fill" style="width:90%;margin:0 auto;background:#e94e24;">加时</a></p>
-    <!--右下角客服跳转按钮-->
-<!--    <div class="fab fab-right-bottom color-purple">
-      &lt;!&ndash;1:定义悬浮按钮UI样式&ndash;&gt;
-      <a href="/abount/">
-        <i class="icon-23453fdf"><img src="../img/kehu.png"></i>
-
-      </a>
-    </div>-->
   </f7-page>
 </template>
 <script>
@@ -74,11 +80,22 @@
     font-size: 14px;
     color: white;
   }
-  .running-state p{
-    font-size: 14px;
-    margin-left: 10px;
+  .running-state{
+    margin-top: 20px;
   }
-  .running-state p span{
+  .running-state table{
+    margin: auto;
+  }
+  .running-state table tr td{
+    padding-top: 10px;
+  }
+  .running-state table tr td:nth-child(1){
+    text-align: right;
+  }
+  .running-state table tr td:nth-child(2){
+    text-align: left;
+  }
+  .running-state table tr td span{
     margin-left: 10px;
   }
 </style>
--- framework7-air-cleaner-customer-vue-webpack-master/src/pages/personal-information.vue
+++ framework7-air-cleaner-customer-vue-webpack-master/src/pages/personal-information.vue
@@ -4,77 +4,55 @@
   <f7-page>
     <f7-navbar class="header-title" title="个人信息设置" back-link="" style="background: #E94E24 !important;"></f7-navbar>
     <f7-list form>
-      <div class="list-block">
-        <ul>
-          <li>
-            <div class="item-content">
-              <div class="item-title label" style="width: 50px">姓名:</div>
-              <div class="item-inner">
-                <div class="item-input">
-                  <input type="text" placeholder="请输入姓名">
-                </div>
-              </div>
+      <div class="list inline-labels no-hairlines-md">
+        <li class="item-content  item-input">
+            <div class="item-title label person" style="width: 35px;">姓名</div>
+            <div class="item-input-wrap" style="width: 100%">
+              <input type="text" placeholder="请输入姓名" required validate>
+              <span class="input-clear-button"></span>
             </div>
-          </li>
-          <li>
-            <div class="item-content">
-              <div class="item-title label" style="width: 50px">年龄:</div>
-              <div class="item-inner">
-                <div class="item-input">
-                  <input type="text" placeholder="18">
-                </div>
-              </div>
+
+        </li>
+        <li class="item-content item-input">
+            <div class="item-title label person" style="width: 35px;">年龄</div>
+            <div class="item-input-wrap">
+              <input type="number" placeholder="请输入年龄">
+              <span class="input-clear-button"></span>
             </div>
-          </li>
-          <li>
-            <div class="item-content">
-              <div class="item-title label" style="width: 50px">性别:</div>
-              <div class="item-inner">
-                <div class="item-input">
-                  <label class="label-switch" style="margin-left: 20px">
-                    <input type="radio" value="1" name="idno">
-                    <div class="checkbox">男</div>
-                  </label>
-                  <label class="label-switch" style="margin-left: 20px">
-                    <input type="radio" value="2" name="idno">
-                    <div class="radio">女</div>
-                  </label>
-                </div>
-              </div>
+        </li>
+        <li class="item-content item-input">
+            <div class="item-title label person" style="width: 35px;">性别</div>
+            <div class="item-input-wrap">
+              <label class="label-switch" style="margin-left: 20px;">
+                <input type="radio" checked value="1" name="idno">
+                <div class="checkbox">男</div>
+              </label>
+              <label class="label-switch" style="margin-left: 20px">
+                <input type="radio" value="2" name="idno">
+                <div class="radio">女</div>
+              </label>
             </div>
-          </li>
-          <li>
-            <div class="item-content">
-              <div class="item-title label" style="width: 50px">昵称:</div>
-              <div class="item-inner">
-                <div class="item-input">
-                  <input type="text" placeholder="昵称">
-                </div>
-              </div>
-            </div>
-          </li>
-          <!-- Date -->
-          <li>
-            <div class="item-content">
-              <div class="item-title label" style="width: 50px">生日:</div>
-              <div class="item-inner">
-                <div class="item-input">
-                  <input type="date" placeholder="请选择日期" value="2014-04-30">
-                </div>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div class="item-content">
-              <div class="item-title label" style="width: 125px">身份证号码:</div>
-              <div class="item-inner">
-                <div class="item-input">
-                  <input type="text" placeholder="请输入身份证号码">
-                </div>
-              </div>
-            </div>
-          </li>
-        </ul>
+        </li>
+        <li class="item-content item-input">
+          <div class="item-title label person" style="width: 35px;">昵称</div>
+          <div class="item-input-wrap" style="width: 100%">
+            <input type="text" placeholder="请输入昵称">
+            <span class="input-clear-button"></span>
+          </div>
+        </li>
+        <li class="item-content item-input">
+          <div class="item-title label person" style="width: 35px;">生日</div>
+          <div class="item-input-wrap" style="width: 100%">
+            <input type="date" value="2014-04-30" placeholder="Please choose...">
+          </div>
+        </li>
+        <li class="item-content  item-input">
+          <div class="item-title label person" style="width: 78px;">身份证号码</div>
+          <div class="item-input-wrap" style="width: 100%">
+            <input type="text" placeholder="请输入身份证号码" required validate>
+            <span class="input-clear-button"></span>
+          </div>
+        </li>
       </div>
       <p><a href="/center/" class="button button-fill" style="width:90%;margin:0 auto;background:#e94e24;">提交</a></p>
     </f7-list>
@@ -84,5 +62,13 @@
 
 </script>
 <style type="text/css">
-
+  input::-webkit-input-placeholder {
+    /* placeholder颜色  */
+    color: #aab2bd;
+    /* placeholder字体大小  */
+    font-size: 12px;
+  }
+  .person {
+    font-size: 12px !important;
+  }
 </style>
