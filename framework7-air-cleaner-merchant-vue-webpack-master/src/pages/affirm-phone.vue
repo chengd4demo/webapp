0df5a8150ad188a75ba67e0740833dd01c007d4d
chengd5demo <229462098@qq.com>
2019-01-03 10:38:34 +0800

    手机号码修改

--- framework7-air-cleaner-merchant-vue-webpack-master/src/pages/affirm-phone.vue
+++ framework7-air-cleaner-merchant-vue-webpack-master/src/pages/affirm-phone.vue
@@ -1,20 +1,29 @@
 <template>
   <f7-page>
-    <f7-navbar title="确认手机号码" back-link="" style="background:#e94e24;"></f7-navbar>
-    <f7-list form style="margin-top:1px">
-      <f7-list-item>
-        <f7-input type="number" placeholder="输入新的手机号码"/>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-col>
-          <f7-input type="number" placeholder="验证码"/>
-        </f7-col>
-        <f7-col>
-          <p><a href="#" id="code" @click="countDown" class= "button button-fill" :class="{disabled: !this.canClick}" no-fast-click style="margin:0 auto;background:#e94e24;disable:disable">{{content}}</a></p>
-        </f7-col>
-      </f7-list-item>
-      <p><a href="#" class="button button-fill" style="width:90%;margin:0 auto;background:#e94e24;">完成</a></p>
-    </f7-list>
+    <f7-navbar title="确认手机号码" back-link="" @click="$f7router.navigate('/home/')" style="background:#e94e24;"></f7-navbar>
+    <form id='phoneInfo' style="margin-top:41px;">
+      <input type="hidden" v-model = "openId" name = "openId"/>
+      <div style="height: 44px; line-height: 44px; width: 100%; background: #fff;border-bottom: 1px solid #ccc;">
+        <input type="number" v-model="phoneNumber" name="phoneNumber" placeholder="输入新的手机号码" @keyup="keyDown()"
+               oninput="if(value.length>11)value=value.slice(0,11)"
+               style="height: 44px; line-height: 44px;  text-indent: 10px;width: 100%;"/>
+      </div>
+      <div style="height: 44px; line-height: 44px; width: 100%; background: #fff;border-bottom: 1px solid #ccc;">
+				<span style="width:70%;float: left;">
+					<input type="number" v-model="verificationCode" name="verificationCode" placeholder="输入验证码" @keyup="keyDown()"
+                 oninput="if(value.length>6)value=value.slice(0,6)"
+                 style="height: 44px; line-height: 44px;  text-indent: 10px;width: 100%;"/>
+				</span>
+        <span style="width: 30%; ">
+					<a href="#" id="code" @click="countDown" class="button button-fill" :class="{disabled: !this.canClick}"
+             no-fast-click style="margin:0 auto;background:#e94e24;disable:disable;height: 44px; line-height:44px;">{{content}}</a>
+				</span>
+      </div>
+      <div style="width: 100%; margin-top: 20px;">
+        <a @click="submitBtn()" class="button button-fill"  :class="{disabled: this.canInput}"
+           style="width:90%;margin:0 auto;background:#e94e24;height: 44px; line-height:44px;">完成</a>
+      </div>
+    </form>
   </f7-page>
 </template>
 
@@ -28,29 +37,98 @@
 </style>
 
 <script>
+  import api from "../network";
+
   export default{
     data() {
       return {
         content: '获取验证码',
         totalTime: 60,
-        canClick:true
+        canClick:true,
+        smsCode: '',
+        oldPhoneNumber:this.$f7route.params.phoneNumber,
+        phoneNumber:'',
+        verificationCode:'',
+        openId:'',
+        userType:"MERCHANT",
+        canInput:true
       }
+    },
+    created() {
+      const self = this;
+      self.init(JSON.parse(localStorage.getItem('USER_INFO')) || {});
     },
     methods: {
       countDown() {
         if (!this.canClick) return
         this.canClick = false
-        this.content = this.totalTime + 's后重新获取'
+        this.content = this.totalTime + 's后重新发送'
         let clock = window.setInterval(() => {
-          this.totalTime --
-          this.content = this.totalTime + 's后重获取'
-          if(this.totalTime <0) {
+          this.totalTime--
+          this.content = this.totalTime + 's后重新发送'
+          if (this.totalTime < 0) {
             window.clearInterval(clock);
-            this.content = '重新获取验证码'
+            this.content = '重新发送验证码'
             this.totalTime = 60
             this.canClick = true
           }
-        },1000)
+        }, 1000)
+      },
+      init(data) {
+        const self = this;
+        self.verificationCode = data.verificationCode
+        self.phoneNumber = data.phoneNumber
+        self.openId = data.weixin
+      },
+      submitBtn(){
+        let toastTop = this.$f7.toast.create({})
+        api.updatePhoneNumber(this.$f7.form.convertToData('#phoneInfo')).then(res => {
+          let data = res.data
+          if (data.status==="200") {
+            this.alertMsg('修改成功')
+            window.setTimeout(()=>{
+              this.$f7router.navigate('/home/')
+            },1000)
+          } else {
+            this.alertMsg('修改失败')
+          }
+        }).catch(err =>{
+          this.alertMsg('服务器繁忙')
+        })
+      },
+      alertMsg( msg ) {
+        let toastTop = this.$f7.toast.create({
+          text: msg,
+          position: 'top',
+          closeTimeout: 1000,
+        })
+        toastTop.open();
+      },
+      keyDown() {
+        if (this.phoneNumber !== "" && this.verificationCode !== "") {
+          if (this.oldPhoneNumber == this.phoneNumber) {
+            this.alertMsg('该号码已绑定，请重新输入手机号码')
+            return
+          }
+          if ( this.verificationCode.length == 6) {
+            if (this.smsCode != this.verificationCode) {
+              this.alertMsg('输入验证码错误,请重新输入')
+              return
+            } else {
+              let nowDate = new Date();
+              let min =  parseInt(nowDate - this.canClickDate) / 1000 / 60;
+              if(1< min) {
+                this.alertMsg('验证码超时,请重新获取')
+                return
+              }
+            }
+          }
+          this.canInput = false
+          //滚动到顶部
+          window.scrollTo(0, 0);
+        } else {
+          this.canInput = true;
+        }
       },
     }
   }
--- framework7-air-cleaner-merchant-vue-webpack-master/src/pages/modify-phone.vue
+++ framework7-air-cleaner-merchant-vue-webpack-master/src/pages/modify-phone.vue
@@ -1,102 +1,131 @@
 <template>
-	<f7-page>
-		<f7-navbar title="修改手机号码" back-link="" style="background:#e94e24;"></f7-navbar>
-		<form>
-			<input type="hidden" v-model="weixin" name="weixin" />
-			<div style="height: 44px; line-height: 44px; width: 100%; background: #fff;border-bottom: 1px solid #ccc;">
-				<input type="number" v-model="phoneNumber" placeholder="输入已绑定的手机号码" oninput="if(value.length>11)value=value.slice(0,11)"
-				 style="height: 44px; line-height: 44px;  text-indent: 10px;width: 100%;" />
-			</div>
-			<div style="height: 44px; line-height: 44px; width: 100%; background: #fff;border-bottom: 1px solid #ccc;">
+  <f7-page>
+    <f7-navbar title="修改手机号码" back-link="" style="background:#e94e24;"></f7-navbar>
+    <form style="margin-top:41px;">
+      <div style="height: 44px; line-height: 44px; width: 100%; background: #fff;border-bottom: 1px solid #ccc;">
+        <input type="number" v-model="phoneNumber" placeholder="输入现在绑定的手机号码" @keyup="keyDown()"
+               oninput="if(value.length>11)value=value.slice(0,11)"
+               style="height: 44px; line-height: 44px;  text-indent: 10px;width: 100%;"/>
+      </div>
+      <div style="height: 44px; line-height: 44px; width: 100%; background: #fff;border-bottom: 1px solid #ccc;">
 				<span style="width:70%;float: left;">
-					<input type="number" v-model="verificationCode" placeholder="输入验证码" oninput="if(value.length>6)value=value.slice(0,6)"
-					 style="height: 44px; line-height: 44px;  text-indent: 10px;width: 100%;" />
+					<input type="number" v-model="verificationCode" placeholder="输入验证码" @keyup="keyDown()"
+                 oninput="if(value.length>6)value=value.slice(0,6)"
+                 style="height: 44px; line-height: 44px;  text-indent: 10px;width: 100%;"/>
 				</span>
-				<span style="width: 30%; ">
+        <span style="width: 30%; ">
 					<a href="#" id="code" @click="countDown" class="button button-fill" :class="{disabled: !this.canClick}"
-					 no-fast-click style="margin:0 auto;background:#e94e24;disable:disable;height: 44px; line-height:44px;">{{content}}</a>
+             no-fast-click style="margin:0 auto;background:#e94e24;disable:disable;height: 44px; line-height:44px;">{{content}}</a>
 				</span>
-			</div>
-			<div style="width: 100%; margin-top: 20px;">
-				<a class="button button-fill" @click="nextBtn(parames)" style="width:90%;margin:0 auto;background:#e94e24;height: 44px; line-height:44px;">下一步</a>
-			</div>
-		</form>
-	</f7-page>
+      </div>
+      <div style="width: 100%; margin-top: 20px;">
+        <a :href="getAffirmHref()" @click = "checkedPhoneNumber()" class="button button-fill"  :class="{disabled: this.canInput}"
+           style="width:90%;margin:0 auto;background:#e94e24;height: 44px; line-height:44px;">下一步</a>
+      </div>
+    </form>
+  </f7-page>
 </template>
 <style>
-	.disabled {
-		background-color: #ddd;
-		border-color: #ddd;
-		color: #57a3f3;
-		cursor: not-allowed;
-	}
+  .disabled {
+    background-color: #ddd;
+    border-color: #ddd;
+    color: #57a3f3;
+    cursor: not-allowed;
+  }
 </style>
 
 <script>
-	import api from '../network'
-	export default {
-		data() {
-			return {
-				content: '获取验证码',
-				totalTime: 60,
-				canClick: true,
-				verificationCode: '',
-				weixin: '',
-				phoneNumber: ''
-			}
-		},
-		created() {
-			const self = this;
-			self.init(JSON.parse(localStorage.getItem('USER_INFO')) || {});
-		},
-		methods: {
-			init(data) {
-				const self = this;
-				self.phoneNumber = data.phoneNumber
-				self.verificationCode = data.verificationCode
-				self.weixin = data.weixin
-				let USER_INFO = JSON.parse(localStorage.getItem('USER_INFO')) || {}
-				console.log(USER_INFO)
-				if (USER_INFO) {
-					self.weixin = USER_INFO.weixin
-				} else {
-					self.alertMsg('手机号错误，请重新输入！')
-				}
-			},
-			countDown() {
-				if (!this.canClick) return
-				this.canClick = false
-				this.content = this.totalTime + 's后重新获取'
-				let clock = window.setInterval(() => {
-					this.totalTime--
-					this.content = this.totalTime + 's后重获取'
-					if (this.totalTime < 0) {
-						window.clearInterval(clock);
-						this.content = '重新获取验证码'
-						this.totalTime = 60
-						this.canClick = true
-					}
-				}, 1000)
-			},
-			nextBtn(parames) {
-				if (this.phoneNumber === "" || this.weixin === "") {
-					return
-				}
-				api.updatePhoneNumber({
-					phoneNumber: this.phoneNumber,
-					verificationCode: this.verificationCode,
-					weixin: this.weixin,
-				}).then(res => {
-					let data = res.data.data;
-					if (res.data.data) {
-						localStorage.setItem('USER_INFO', JSON.stringify(data))
-						this.$f7router.back('/home/')
-						this.$f7router.url = '/affirm-phone/'
-					}
-				}).catch(err => {
-					alert('服务器繁忙')
-				})
-			},
-		}
-	}
+  import api from "../network";
+
+  export default {
+    data() {
+      return {
+        content: '发送验证码',
+        totalTime: 60,
+        canClick: true,
+        smsCode: '111111',
+        verificationCode: '',
+        phoneNumber: '',
+        canInput:true,
+        canClickDate:new Date()
+      }
+    },
+    methods: {
+      countDown() {
+        if (!this.canClick) return
+        this.canClickDate = new Date()
+        this.canClick = false
+        this.content = this.totalTime + 's后重新发送'
+        let clock = window.setInterval(() => {
+          this.totalTime--
+          this.content = this.totalTime + 's后重新发送'
+          if (this.totalTime < 0) {
+            window.clearInterval(clock);
+            this.content = '重新发送验证码'
+            this.totalTime = 60
+            this.canClick = true
+          }
+        }, 1000)
+      },
+      updatePhoneNumber(params) {
+        var self = this;
+        api.updatePhoneNumber(params).then(function (res) {
+          var data = res.data.data;
+          self.verificationCode = data.verificationCode
+          self.phoneNumber = data.phoneNumber
+        }).catch(function(err){
+          console.log(err+'sss')
+        })
+      },
+      alertMsg( msg ) {
+        let toastTop = this.$f7.toast.create({
+          text: msg,
+          position: 'top',
+          closeTimeout: 1000,
+        })
+        toastTop.open();
+      },
+      keyDown() {
+        if (this.phoneNumber !== "" && this.verificationCode !== "") {
+          if (this.phoneNumber.length == 11) {
+            if (!this.checkedPhoneNumber(this.phoneNumber)) {
+              this.alertMsg('请输入绑定的手机号码')
+              return
+            }
+          }
+          if ( this.verificationCode.length == 6) {
+            if (this.smsCode != this.verificationCode) {
+              this.alertMsg('输入验证码错误,请重新输入')
+              return
+            } else {
+              let nowDate = new Date();
+              let min =  parseInt(nowDate - this.canClickDate) / 1000 / 60;
+              if(1< min) {
+                this.alertMsg('验证码超时,请重新获取')
+                return
+              } else {
+                this.canInput = false;
+              }
+            }
+          }
+          //滚动到顶部
+          window.scrollTo(0, 0);
+        } else {
+          this.canInput = true;
+        }
+      },
+      checkedPhoneNumber(phoneNumber){
+        //调用接口认证成功返回true
+        let USER_INFO = JSON.parse(localStorage.getItem('USER_INFO')) || {}
+        if (!phoneNumber || phoneNumber !== USER_INFO.phoneNumber) {
+          return false;
+        }
+        return true;
+      },
+      getAffirmHref() {
+        return '/affirm-phone/verificationCode/' + this.verificationCode + '/phoneNumber/' + this.phoneNumber + '/';
+      }
+    }
+  }
+
 </script>
