76e32784368a2d2ee336dafb77db2b8e19a4fa88
kaedeliu <88526743@qq.com>
2019-03-09 19:57:19 +0800

    新手机号码绑定短信校验

--- framework7-air-cleaner-merchant-vue-webpack-master/src/pages/affirm-phone.vue
+++ framework7-air-cleaner-merchant-vue-webpack-master/src/pages/affirm-phone.vue
@@ -45,10 +45,10 @@
         content: '获取验证码',
         totalTime: 60,
         canClick:true,
-        smsCode: '222222',
+        smsCode: '111111',
         oldPhoneNumber:this.$f7route.params.phoneNumber,
         phoneNumber:'',
-        verificationCode:'222222',
+        verificationCode:'',
         openId:'',
         userType:"MERCHANT",
         canInput:true
@@ -60,6 +60,15 @@
     },
     methods: {
       countDown() {
+				window.scrollTo(0, 0);
+				if(this.phoneNumber.length ==0 ){
+				  this.alertMsg('请输入手机号码')
+				  return
+				} else if(this.phoneNumber.length !=0 && this.phoneNumber.length<11) {
+				  this.alertMsg('请输入正确的手机号码')
+				  return
+				}
+				this.querySmsCode(this.phoneNumber)
         if (!this.canClick) return
         this.canClick = false
         this.content = this.totalTime + 's后重新发送'
@@ -76,7 +85,7 @@
       },
       init(data) {
         const self = this;
-        self.verificationCode = data.verificationCode
+        self.verificationCode = ''
         self.phoneNumber = ''
         self.openId = data.weixin
       },
@@ -84,6 +93,8 @@
         let toastTop = this.$f7.toast.create({})
         api.updatePhoneNumber(this.$f7.form.convertToData('#phoneInfo')).then(res => {
           let data = res.data
+					self.verificationCode = data.verificationCode
+					self.phoneNumber = data.phoneNumber
           if (data.status === "200") {
             this.alertMsg('修改成功')
             window.setTimeout(()=>{
@@ -105,29 +116,66 @@
         toastTop.open();
       },
       keyDown() {
-        if (this.phoneNumber !== "" && this.verificationCode !== "" && this.phoneNumber.length === 11 && this.verificationCode.length === 6) {
-          if (this.oldPhoneNumber === this.phoneNumber) {
+        if (this.phoneNumber != "" && this.verificationCode != "" && this.phoneNumber.length == 11 && this.verificationCode.length == 6) {
+          if (this.oldPhoneNumber == this.phoneNumber) {
             this.alertMsg('该号码已绑定，请重新输入手机号码')
             return
           }
-          if (this.smsCode !== this.verificationCode) {
+          if (this.smsCode != this.verificationCode) {
             this.alertMsg('输入验证码错误,请重新输入')
             return
-          } else {
-            let nowDate = new Date();
-            let min =  parseInt(nowDate - this.canClickDate) / 1000 / 60;
-            if(1< min) {
-              this.alertMsg('验证码超时,请重新获取')
-              return
-            }
           }
-          this.canInput = false
           //滚动到顶部
           window.scrollTo(0, 0);
+          if(this.verificationCode.length == 6 &&  this.phoneNumber.length==11) {
+          	this.checkValidVerificationCode()
+          }
         } else {
           this.canInput = true;
         }
       },
+			checkValidVerificationCode (){
+				if (this.smsCode!=this.verificationCode) {
+					this.alertMsg("输入验证码错误,请重新输入")
+					return
+				}
+				api.checkValidVerificationCode({
+						phoneNumber:this.phoneNumber,
+					  inVerificationCode:this.verificationCode,
+						verificationCode:this.smsCode
+					}).then(res => {
+							let data = res.data
+							if (data.status != '200'){
+									if (data.status == 'EP500') {
+										this.alertMsg("服务繁忙")
+									} else {
+										this.alertMsg(data.description)
+									}
+									return
+							} else {
+									 this.canInput = false
+							}
+					}).catch(err => {
+							this.alertMsg("服务繁忙")
+					})
+			},
+			querySmsCode(phoneNumber) {
+				let parames = phoneNumber + '?smsType=chagePhone'
+				api.sendSms(parames).then(res => {
+					if (res.data.status == '200' && res.data.data) {
+						this.smsCode = res.data.data
+						this.alertMsg('短信发送成功')
+					} else {
+						if (res.data.status == 'EP500') {
+							this.alertMsg('服务器繁忙!')
+						} else if (res.data.status == '1013' || res.data.status == '1014' || res.data.status == '1015') {
+							this.alertMsg(res.data.description)
+						}
+					}
+				}).catch(err => {
+					this.alertMsg('发送短信失败')
+				})
+			}
     }
   }
 
